9cce30e58a32eaf6c3edb22d52f44da9e3d59d9d
a422916528 <422916528@qq.com>
2023-06-20 19:52:43 +0800

    feat: 登录表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,12 @@
 <!-- eslint-disable no-undef -->
 <script setup lang="ts">
-  import { User, Lock } from '@element-plus/icons-vue'
+  import { User, Lock, Warning } from '@element-plus/icons-vue'
   import { ref } from 'vue'
   import { useUserStore } from '@/stores/user'
   import { useRouter } from 'vue-router'
   import { getTime } from '@/composable/useTime'
   // 登录表单数据
+  const refLoginForm = ref(null)
   const loginForm = ref({
     username: 'admin',
     password: '111111'
@@ -15,28 +16,49 @@
   const router = useRouter()
   // 登录回调
   const login = () => {
-    userStore.userLogin(loginForm.value).then(
-      () => {
-        // 登录成功
-        ElNotification({
-          type: 'success',
-          message: '登录成功，欢迎回来',
-          title: `Hi ${timeMessage.value}`
-        })
-        router.push({ name: 'home' })
-      },
-      // 登录失败
-      error => {
-        ElNotification({
-          type: 'error',
-          message: error.message
+    refLoginForm.value.validate((isValid: boolean) => {
+      if (isValid) {
+        // 校验通过
+        userStore.userLogin(loginForm.value).then(
+          () => {
+            // 登录成功
+            ElNotification({
+              type: 'success',
+              message: '登录成功，欢迎回来',
+              title: `Hi ${timeMessage.value}`
+            })
+            router.push({ name: 'home' })
+          },
+          // 登录失败
+          error => {
+            ElNotification({
+              type: 'error',
+              message: error.message
+            })
+          }
+        )
+      } else {
+        ElMessage({
+          type: 'warning',
+          message: '账户或密码格式有误'
         })
       }
-    )
+    })
   }
   // 获取当前时间区域
   const timeMessage = ref('')
   timeMessage.value = getTime()
+  // 登录表单校验
+  const rules = ref({
+    username: [
+      { required: true, message: '用户名不能为空', trigger: 'blur' },
+      { min: 5, max: 10, message: '长度为 6 到 10 ', trigger: 'blur' }
+    ],
+    password: [
+      { required: true, message: '密码不能为空', trigger: 'blur' },
+      { min: 6, max: 10, message: '长度为 6 到 10', trigger: 'blur' }
+    ]
+  })
 </script>
 
 <template>
@@ -44,13 +66,13 @@
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form" :model="loginForm">
+        <el-form class="login_form" :model="loginForm" :rules="rules" ref="refLoginForm">
           <h1>Hello</h1>
           <h2>欢迎来到硅谷甄选</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               type="password"
               show-password
