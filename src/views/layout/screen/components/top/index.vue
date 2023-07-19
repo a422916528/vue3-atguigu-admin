328542a1b845c26e3961a043ec522c49c31ce181
a422916528 <422916528@qq.com>
2023-07-19 10:53:45 +0800

    feat: 数据大屏-顶部完成

---
+++ src/views/layout/screen/components/top/index.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+  import { ref, onMounted, onUnmounted } from 'vue'
+  import { useRouter } from 'vue-router'
+  import dayjs from 'dayjs'
+  const router = useRouter()
+
+  const time = ref('')
+  let timer = null
+  onMounted(() => {
+    time.value = dayjs().format('YYYY-MM-DD HH:mm:ss')
+    timer = setInterval(() => {
+      time.value = dayjs().format('YYYY-MM-DD HH:mm:ss')
+    }, 1000)
+  })
+  onUnmounted(() => {
+    clearInterval(timer)
+  })
+</script>
+
+<template>
+  <div class="top">
+    <div class="left">
+      <span class="lf-btn" @click="router.push({ path: '/home' })">首页</span>
+    </div>
+    <div class="center">
+      <div class="title">智慧旅游可视化大数据平台</div>
+    </div>
+    <div class="right">
+      <span class="rt_btn">统计报告</span>
+      <span class="rt_time">当前时间：{{ time }}</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .top {
+    width: 100%;
+    height: 40px;
+    display: flex;
+    .left {
+      flex: 1;
+      background: url('@/assets/images/screen/dataScreen-header-left-bg.png') no-repeat;
+      background-size: cover;
+      .lf-btn {
+        width: 150px;
+        height: 40px;
+        float: right;
+        background: url('@/assets/images/screen/dataScreen-header-btn-bg-l.png') no-repeat;
+        background-size: 100% 100%;
+        text-align: center;
+        line-height: 40px;
+        color: #29fcff;
+        font-size: 20px;
+        cursor: pointer;
+      }
+    }
+    .center {
+      flex: 2;
+      .title {
+        width: 100%;
+        height: 74px;
+        background: url('@/assets/images/screen/dataScreen-header-center-bg.png') no-repeat;
+        background-size: 100% 100%;
+        text-align: center;
+        line-height: 74px;
+        font-size: 36px;
+        color: #29fcff;
+      }
+    }
+    .right {
+      flex: 1;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      background: url('@/assets/images/screen/dataScreen-header-left-bg.png') no-repeat;
+      background-size: cover;
+      .rt_btn {
+        width: 150px;
+        height: 40px;
+        background: url('@/assets/images/screen/dataScreen-header-btn-bg-r.png') no-repeat;
+        background-size: 100% 100%;
+        text-align: center;
+        line-height: 40px;
+        color: #29fcff;
+        font-size: 20px;
+        cursor: pointer;
+      }
+      .rt_time {
+        color: #29fcff;
+        font-size: 20px;
+        margin-right: 10px;
+      }
+    }
+  }
+</style>
--- src/views/layout/screen/index.vue
+++ src/views/layout/screen/index.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
-  import {} from 'vue'
+  import { ref, onMounted } from 'vue'
+  import Top from './components/top/index.vue'
+  // #region scale 适配
+  const screenRef = ref<HTMLElement>(null)
+  function getScale(w = 1920, h = 1080) {
+    const ww = window.innerWidth / w
+    const wh = window.innerHeight / h
+    return ww < wh ? ww : wh
+  }
+  onMounted(() => {
+    screenRef.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`
+  })
+  window.addEventListener('resize', () => {
+    screenRef.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`
+  })
+  // #endregion
 </script>
 
 <template>
   <div class="container">
-    <h1>数据大屏</h1>
+    <div class="screen" ref="screenRef">
+      <Top></Top>
+      <div class="main">
+        <div class="left">左</div>
+        <div class="center">中</div>
+        <div class="right">右</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +37,27 @@
     left: 0;
     width: 100vw;
     height: 100vh;
-    background-color: orange;
+    background: url('@/assets/images/screen/bg.png') no-repeat;
+    background-size: cover;
+    .screen {
+      width: 1920px;
+      height: 1080px;
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform-origin: left top;
+      .main {
+        display: flex;
+        .left {
+          flex: 1;
+        }
+        .center {
+          flex: 2;
+        }
+        .right {
+          flex: 1;
+        }
+      }
+    }
   }
 </style>
